515d696e3de1e9f50838e73627ff18e84320ea5f
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-08-28 15:33:22 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -71,7 +71,7 @@ async def start(bot, update):
         InlineKeyboardButton('ᴄʜᴀɴɴᴇʟ', url='https://t.me/movieslokam2'),
         InlineKeyboardButton('ɢʀᴏᴜᴘ', url='https://t.me/mazhatthullikal')
     ],[
-        InlineKeyboardButton('ʜᴇʟᴘ', callback_data="about"), 
+        InlineKeyboardButton('ʜᴇʟᴘ', callback_data="close"), 
         InlineKeyboardButton('ᴄʟᴏꜱᴇ', callback_data='close')
     ]]
     
@@ -87,40 +87,3 @@ async def start(bot, update):
     )
 
 
-@Client.on_message(filters.command(["help"]) & filters.private, group=1)
-async def help(bot, update):
-    buttons = [[
-        InlineKeyboardButton('ʜᴏᴍᴇ ', callback_data='start'),
-        InlineKeyboardButton('ᴀʙᴏᴜᴛ ', callback_data='about')
-    ],[
-        InlineKeyboardButton('ᴄʟᴏꜱᴇ ', callback_data='close')
-    ]]
-    
-    reply_markup = InlineKeyboardMarkup(buttons)
-    
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.HELP_TEXT,
-        reply_markup=reply_markup,
-        parse_mode="html",
-        reply_to_message_id=update.message_id
-    )
-
-
-@Client.on_message(filters.command(["about"]) & filters.private, group=1)
-async def about(bot, update):
-    
-    buttons = [[
-        InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
-        InlineKeyboardButton('ᴄʟᴏꜱᴇ', callback_data='close')
-    ]]
-    reply_markup = InlineKeyboardMarkup(buttons)
-    
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.ABOUT_TEXT,
-        reply_markup=reply_markup,
-        disable_web_page_preview=True,
-        parse_mode="html",
-        reply_to_message_id=update.message_id
-    )
